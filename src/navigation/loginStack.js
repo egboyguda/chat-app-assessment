1900c617c3e866c1f91c04bc45abc0c5730c333a
egboyguda <e.gboyguda@gmail.com>
2023-05-14 08:20:21 +0800

    added register screen

--- src/navigation/loginStack.js
+++ src/navigation/loginStack.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/login.screen";
+import RegisterScreen from "../screens/register.screen";
 
 const Stack = createNativeStackNavigator();
 const LoginStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
+      <Stack.Screen name="Register" component={RegisterScreen}></Stack.Screen>
     </Stack.Navigator>
   );
 };
--- src/screens/register.screen.js
+++ src/screens/register.screen.js
@@ -1,10 +1,9 @@
-import { Button } from "@rneui/base";
-import { Input } from "@rneui/themed";
 import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const LoginScreen = ({ navigation }) => {
+import { Image, Text } from "react-native";
+import { Input } from "@rneui/themed";
+import { Button } from "@rneui/base";
+const RegisterScreen = () => {
   return (
     <SafeAreaView
       style={{
@@ -16,9 +15,9 @@ const LoginScreen = ({ navigation }) => {
     >
       <Image
         source={require("../../assets/chat.png")}
-        style={{ height: 250, width: 250, marginBottom: 10 }}
+        style={{ height: 250, width: 250 }}
       />
-      <Text style={{ fontSize: 24, fontWeight: "bold" }}>CHAT APP</Text>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>Register</Text>
       <Input
         placeholder="username"
         containerStyle={{
@@ -42,33 +41,41 @@ const LoginScreen = ({ navigation }) => {
           width: "80%",
           borderWidth: 1,
           height: 48,
+          marginBottom: 10,
         }}
         inputContainerStyle={{
           borderBottomWidth: 0,
           //paddingTop: 10,
         }}
+
+        //inputStyle={{ textAlignVertical: "center" }}
+      />
+      <Input
+        placeholder="retype password"
+        containerStyle={{
+          borderRadius: 30,
+          width: "80%",
+          borderWidth: 1,
+          height: 48,
+          marginBottom: 10,
+        }}
+        inputContainerStyle={{
+          borderBottomWidth: 0,
+          //paddingTop: 10,
+        }}
+
         //inputStyle={{ textAlignVertical: "center" }}
       />
       <Button
-        title={"Login"}
-        containerStyle={{
-          width: "80%",
-          borderRadius: 30,
-          marginVertical: 10,
-        }}
-      />
-      <Button
-        title={"Register"}
+        title={"Submit"}
         containerStyle={{
           width: "80%",
           borderRadius: 30,
           marginVertical: 5,
         }}
-        onPress={() => {
-          navigation.navigate("Register");
-        }}
       />
     </SafeAreaView>
   );
 };
-export default LoginScreen;
+
+export default RegisterScreen;
