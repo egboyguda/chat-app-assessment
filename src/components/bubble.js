ce4e193ed85416142c85a9a96424547379d8aad1
egboyguda <e.gboyguda@gmail.com>
2023-05-30 23:26:12 +0800

    modifying bubble to make it appeling,editing chatbox to pass prop to bubble

--- src/components/bubble.js
+++ src/components/bubble.js
@@ -1,26 +1,76 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Avatar } from "@rneui/base";
 
-const Bubble = ({ message }) => {
+const Bubble = ({ message, isUser, username }) => {
+  const containerStyle = isUser
+    ? styles.userContainer
+    : styles.defaultContainer;
+  const messageStyle = isUser ? styles.userMessage : styles.defaultMessage;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.message}>{message}</Text>
+    <View style={[styles.container, containerStyle]}>
+      {!isUser && (
+        <View style={styles.avatarContainer}>
+          <Avatar
+            rounded
+            icon={{ name: "user", type: "font-awesome" }}
+            containerStyle={styles.avatar}
+          />
+        </View>
+      )}
+      <View style={styles.messageContainer}>
+        {username && <Text style={styles.username}>{username}</Text>}
+        <Text style={[styles.message, messageStyle]}>{message}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-    backgroundColor: "#ACBCFF",
-    borderRadius: 10,
+    borderRadius: 20,
     marginHorizontal: 10,
     marginVertical: 5,
+    alignSelf: "flex-start",
+    maxWidth: "80%",
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    backgroundColor: "#E0E0E0",
+    flexDirection: "row",
+  },
+  defaultContainer: {
+    alignSelf: "flex-start",
+  },
+  userContainer: {
+    alignSelf: "flex-end",
+    backgroundColor: "#2979FF",
+  },
+  avatarContainer: {
+    marginRight: 8,
+    alignSelf: "flex-end",
+  },
+  avatar: {
+    width: 32,
+    height: 32,
+    backgroundColor: "#ACBCFF",
+  },
+  messageContainer: {
+    flex: 1,
+  },
+  username: {
+    marginBottom: 4,
+    fontSize: 12,
+    color: "#808080",
   },
   message: {
     fontSize: 16,
     lineHeight: 20,
     color: "#000000",
+  },
+  defaultMessage: {},
+  userMessage: {
+    color: "#FFFFFF",
   },
 });
 
--- src/screens/chatBox.js
+++ src/screens/chatBox.js
@@ -1,31 +1,23 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-  View,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
-  FlatList,
-  Text,
-  StyleSheet,
-  TextInput,
-} from "react-native";
+import { View, FlatList, Text, StyleSheet, TextInput } from "react-native";
 import { Input, Button } from "@rneui/themed";
 import { Ionicons } from "@expo/vector-icons";
 import Bubble from "../components/bubble";
 
 import { Context as ApiContext } from "../context/apiContext";
-//import { SafeAreaView } from "react-native-safe-area-context";
+import { Context as AuthContext } from "../context/authContext";
 
-//const [reciever, setReciever] = useState(msg[0]._id);
 const ChatBox = ({ route }) => {
   const [reply, setReply] = useState("");
+  const {
+    state: { username },
+  } = useContext(AuthContext);
   const {
     state: { chat },
     sendMsg,
   } = useContext(ApiContext);
   const { msg } = route.params;
 
-  //console.log(chat.find((item) => item._id === msg));
   return (
     <View style={styles.container}>
       <FlatList
@@ -36,7 +28,13 @@ const ChatBox = ({ route }) => {
             .reverse() || []
         }
         keyExtractor={(item) => item.timestamp}
-        renderItem={({ item }) => <Bubble message={item.message} />}
+        renderItem={({ item }) => (
+          <Bubble
+            message={item.message}
+            isUser={item.sender === username}
+            username={item.sender !== username ? item.sender : null}
+          />
+        )}
         inverted
       />
       <View style={styles.inputContainer}>
@@ -63,26 +61,26 @@ const ChatBox = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  bubble: {
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 10,
-    backgroundColor: "#F2F2F2",
+    backgroundColor: "#FFFFFF",
   },
   inputContainer: {
     flexDirection: "row",
     alignItems: "center",
     padding: 10,
+    borderTopWidth: 1,
+    borderTopColor: "#CCCCCC",
+    backgroundColor: "#F5F5F5",
   },
   input: {
     flex: 1,
     marginRight: 10,
-    paddingVertical: 5,
-    paddingHorizontal: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
     borderWidth: 1,
     borderColor: "#CCCCCC",
-    borderRadius: 10,
+    borderRadius: 20,
+    backgroundColor: "#FFFFFF",
   },
 });
+
 export default ChatBox;
